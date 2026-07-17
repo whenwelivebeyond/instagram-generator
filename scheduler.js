08b395c0e4741fcb05259aa516045ecb2c6cb3fc
whenwelivebeyond <when.we.live.beyond@gmail.com>
2026-07-17 19:49:17 +0530

    scheduler added
    1st step of publishing scheduler

---
+++ scheduler.js
@@ -0,0 +1,4 @@
+// This page is intentionally standalone while the scheduler is being built.
+// Future milestones will add the secure queue and Instagram connection here.
+const queueCount = document.querySelector("#queueCount");
+queueCount.textContent = "0 posts";
